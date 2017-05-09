a363cadffeceb63dc3e80bd0339acbe7ac057d4f
brett-davi5 <brettdxvi@gmail.com>
2017-05-08 21:30:27 -0500

    Add files via upload

---
+++ Bird.java
@@ -0,0 +1,67 @@
+package com.flappydemo.game.Sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ * Created by Family on 10/2/2016.
+ */
+
+public class Bird {
+
+    private static final int GRAVITY = -15;
+    private static final int MOVEMENT = 100;
+    private Vector3 position;
+    private Vector3 velocity;
+    private Animation birdAnimation;
+    private Texture texture;
+
+    private Rectangle bounds;
+
+    public Bird(int x, int y){
+        position = new Vector3(x, y, 0);
+        velocity = new Vector3(0, 0, 0);
+        texture = new Texture("birdanimation.png");
+        birdAnimation = new Animation(new TextureRegion(texture), 3, 0.5f);
+        bounds = new Rectangle(x, y, texture.getWidth() / 3, texture.getHeight());
+
+    }
+
+    public void update(float dt){
+        birdAnimation.update(dt);
+        if(position.y > 0)
+            velocity.add(0, GRAVITY, 0);
+
+        velocity.scl(dt);
+        position.add(MOVEMENT * dt, velocity.y, 0);
+        velocity.scl(1/dt);
+        if(position.y < 0)
+            position.y = 0;
+        bounds.setPosition(position.x, position.y);
+
+    }
+
+    public Vector3 getPosition() {
+        return position;
+    }
+
+    public TextureRegion getTexture() {
+        return birdAnimation.getFrame();
+    }
+
+    public void jump(){
+        velocity.y = 250;
+    }
+
+    public Rectangle getBounds(){
+        return bounds;
+
+    }
+
+    public void dispose(){
+        texture.dispose();
+    }
+
+}
